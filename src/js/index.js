68a9d9afeb886ff18bed79e0e49768f3e713e20f
yymmzzz <chi1127821@gmail.com>
2024-12-06 15:50:25 +0900

    ♻️  : [코드 수정] react-dom 랜더링 오류 해결 → react-dom/client

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,13 @@
-import React from "react";
+/* import React from "react";
 import ReactDom from "react-dom";
-import App from "./App";
 
-ReactDom.render(<App />, document.getElementById("root"));
+ReactDom.render(<h1>Hello React App</h1>, document.getElementById("root"));
+ */
+
+import React from "react";
+import ReactDOM from "react-dom/client"; // React 18의 새로운 렌더링 API
+
+const rootElement = document.getElementById("root");
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(<h1>Hello React App</h1>);
--- webpack.common.js
+++ webpack.common.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 module.exports = {
   mode: "development",
-  entry: "/src/js/index.js",
+  entry: "./src/js/index.js",
   devtool: "inline-source-map",
   target: "electron-renderer",
   module: {
